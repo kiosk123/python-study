953ebf40085c47a132ab0580bef0e4ac8e3bede0
Heo Jong Tae <heo9910@naver.com>
2020-12-25 23:28:49 +0900

    .

---
+++ chapter-01-basic/02_calendar.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*
+'''
+Created on 2020. 12. 25.
+
+@author: USER
+'''
+import calendar
+
+# 일요일을 첫요일에 위치
+calendar.setfirstweekday(6)
+calendar.prmonth(2012, 6)
+
+#      June 2012
+# Su Mo Tu We Th Fr Sa
+#                 1  2
+#  3  4  5  6  7  8  9
+# 10 11 12 13 14 15 16
+# 17 18 19 20 21 22 23
+# 24 25 26 27 28 29 30
