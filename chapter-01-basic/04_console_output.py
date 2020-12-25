455aa60148ddc31e1830c579db20b68e33c44fbc
Heo Jong Tae <heo9910@naver.com>
2020-12-26 00:21:18 +0900

    .

---
+++ chapter-01-basic/04_console_output.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*
+'''
+Created on 2020. 12. 26.
+
+@author: USER
+'''
+# -*- coding: utf-8 -*
+import math
+'''
+Created on 2020. 12. 26.
+
+@author: USER
+'''
+
+# add : 9 sub = 2
+print('add :', 4 + 5, 'sub =', 4 - 2)
+
+# 1 2
+# 3 4
+print(1, 2); print(3, 4)
+
+# 1 2 3 4
+print(1, 2, end = ' '); print(3, 4)
+
+# 1 2 3 4 5
+print(1, 2, 3, 4, 5)
+
+# 1,2,3,4,5
+print(1, 2, 3, 4, 5, sep=",") 
+
+# file 객체를 통해서 파일로 출력
+f = open("./data/out.txt", 'w')
+print(1, 2, 3, 4, 5, file = f)
+f.close()
+
+# 제곱근 출력
+for k in range(1, 5):
+    print('sqrt ({0})={1}'.format(k, math.sqrt(k)))
+    
+
+# 소수점 자리수를 3자리고 고정한 후 출력 {.을포함한 전체자릿수.소수점자리수f}
+for k in range(1, 5):
+    print('sqrt ({0})={1:5.3f}'.format(k, math.sqrt(k)))
+    
+    
+# 데이터를 구조화하여 좀더 깔끔하게 출력
+import pprint
+complicated = ['spame', (1,2,3), ('ham', 'egg', ('ab', 'cd', ('abc', 'def')))]
+complicated = complicated * 3
+pprint.pprint(complicated)
+
+'''
+실행 결과
+['spame',
+ (1, 2, 3),
+ ('ham', 'egg', ('ab', 'cd', ('abc', 'def'))),
+ 'spame',
+ (1, 2, 3),
+ ('ham', 'egg', ('ab', 'cd', ('abc', 'def'))),
+ 'spame',
+ (1, 2, 3),
+ ('ham', 'egg', ('ab', 'cd', ('abc', 'def')))]
+'''
+    
