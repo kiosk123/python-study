58684e3c05c83b77aa1293a693afef6f10f3d349
Heo Jong Tae <heo9910@naver.com>
2020-12-25 23:18:09 +0900

    .

---
+++ chapter-01-basic/01_arithmetic.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*
+'''
+Created on 2020. 12. 25.
+
+@author: USER
+'''
+result = 4 + 5
+print(result) #9
+
+result = 12 -32
+print(result) #
+
+result = (4 + 5) * 6
+print(result) 
+
+result = 3 / 2 # 나누기 - 결과는 소수
+print(result)  # 1.5
+
+result = 3 // 2 # 몫만
+print(result) 
+
+result = 9 % 5 # 나머지
+print(result) 
+
+
