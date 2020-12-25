64ecde187a6beb1337c91a768ae70934fd2ee781
Heo Jong Tae <heo9910@naver.com>
2020-12-25 23:25:08 +0900

    .

--- chapter-01-basic/01_arithmetic.py
+++ chapter-01-basic/01_arithmetic.py
@@ -22,4 +22,19 @@ print(result)
 result = 9 % 5 # 나머지
 print(result) 
 
+result = divmod(9, 5) # 나머지와 몫을 한꺼번에 계산
+print(result)
+
+# 복소수 연산
+op1 = 1 + 4j
+op2 = 5 - 3j
+result = op1 * op2
+print(result)
+
+# cmath 모듈을 이용한 복소수 연산
+from cmath import *
+print(cos(pi / 3), sin(pi / 3)) # (0.5000000000000001-0j) (0.8660254037844386+0j)
+
+result = sqrt(-2)
+print(result) # 1.4142135623730951j
 
